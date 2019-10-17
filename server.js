5f8481c7f7757d79317a8b84675c04a66489f6c7
Josiah Mortenson <josiah.mortenson@gmail.com>
2019-10-16 19:50:02 -0500

    added routes for pets

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const cors = require('cors');
 const https = require('https');
 const port = process.env.PORT || 3000;
 const { errorHandler } = require('./handlers/error');
+const {
+  createPet,
+  getPet,
+  getOwnersPets,
+  updatePet,
+  deletePet,
+} = require('./handlers/pets');
 const { login, signup, recallUser, logout } = require('./handlers/auth');
 
 const app = express();
@@ -14,6 +21,13 @@ app.post('/api/auth/login', login);
 app.post('/api/auth/signup', signup);
 app.post('/api/auth/recall', recallUser);
 app.get('/api/auth/logout', logout);
+
+// pets
+app.get('/api/pets/:pet_id', getPet);
+app.get('/api/users/:user_email/pets', getOwnersPets);
+app.post('/api/pets', createPet);
+app.put('/api/pets/:pet_id', updatePet);
+app.delete('/api/pets/:pet_id', deletePet);
 
 // ==================
 // === Catch 404s ===
