df5aad30bf7f328e646d737858498c5d2c0434f9
Josiah Mortenson <josiah.mortenson@gmail.com>
2020-01-06 21:36:39 -0600

    all routes are now authenticatedRoutes.  this is the first wave of locking down the api

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const { authenticatedRoute } = require('../middleware/auth');
 const router = express.Router();
 
 router.use('/api/auth', require('./auth'));
-router.use('/api/appts', require('./appointments'));
-router.use('/api/loc', require('./locations'));
-router.use('/api/org', require('./organizations'));
-router.use('/api/pets', require('./pets'));
-router.use('/api/users', require('./users'));
+router.use('/api/appts', authenticatedRoute, require('./appointments'));
+router.use('/api/loc', authenticatedRoute, require('./locations'));
+router.use('/api/org', authenticatedRoute, require('./organizations'));
+router.use('/api/pets', authenticatedRoute, require('./pets'));
+router.use('/api/users', authenticatedRoute, require('./users'));
 
 module.exports = router;
