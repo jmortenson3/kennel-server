d9dbcd5c6a2e38643afeec2c6058a10cf125cb18
Josiah Mortenson <josiah.mortenson@gmail.com>
2020-01-23 21:28:10 -0600

    business logic pulled out into locationService

--- src/routes/locations.ts
+++ src/routes/locations.ts
@@ -1,138 +1,107 @@
 import express, { Request, Response, NextFunction } from 'express';
 
+import { ILocation } from '../interfaces/ILocation';
 import { nowISO } from '../utils';
 import db from '../db';
+import { LocationService } from '../services/location';
 
 const router = express.Router();
 
 router.post('/', async (req: Request, res: Response, next: NextFunction) => {
-  const { loc_name, org_id } = req.body;
-  if (!org_id || !loc_name) {
-    return next({ error: 'org_id or loc_name is not defined' });
-  }
   try {
-    let query =
-      'INSERT INTO locations ' +
-      '(org_id, loc_name, created_datetime, updated_datetime) ' +
-      'VALUES ($1, $2, $3, $4) ' +
-      'RETURNING id;';
-    const { rows } = await db.query(query, [
-      org_id,
-      loc_name,
-      nowISO(),
-      nowISO(),
-    ]);
-    res.status(200).json({ data: rows[0] });
+    const { loc_name, org_id } = req.body;
+
+    if (!org_id || !loc_name) {
+      throw new Error('org_id or loc_name is not defined');
+    }
+
+    const locationService = new LocationService();
+    const orgId = org_id.trim();
+    const locName = loc_name.trim();
+    const location = await locationService.CreateLocation(locName, orgId);
+    res.status(200).json({ data: location });
   } catch (err) {
-    console.log(err.message);
-    next(err);
+    next({ message: err, statusCode: 400 });
   }
 });
 
 router.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
-  const id = req.params.id;
-
-  if (!id) {
-    return next({ error: 'location id is not defined' });
-  }
-
   try {
-    let query =
-      'SELECT id, org_id, loc_name, created_datetime, updated_datetime ' +
-      'FROM locations ' +
-      'WHERE id = $1;';
-    const { rows } = await db.query(query, [id]);
-    res.status(200).json({ data: rows });
+    const id = req.params.id;
+
+    if (!id) {
+      throw new Error('location id is not defined');
+    }
+
+    const locationId = id.trim();
+    const locationService = new LocationService();
+    const location = await locationService.GetLocation(locationId);
+    res.status(200).json({ data: location });
   } catch (err) {
     console.log(err);
-    next(err);
+    next({ message: err, statusCode: 400 });
   }
 });
 
 router.get('/', async (req: Request, res: Response, next: NextFunction) => {
   try {
-    let rows;
-    if (req.query.org_id) {
-      rows = await getLocByOrg(req.query.org_id);
-    } else {
-      rows = await getLocs();
+    if (!req.query.org_id) {
+      throw new Error('must specifiy org_id in query');
     }
-    res.status(200).json({ data: rows });
+    const orgId = req.query.org_id.trim();
+    const locationService = new LocationService();
+    const locations = await locationService.GetLocationsByOrgId(orgId);
+    res.status(200).json({ data: locations });
   } catch (err) {
-    next(err);
+    next({ message: err, statusCode: 400 });
   }
 });
 
-let getLocs = async () => {
-  let query =
-    'SELECT id, org_id, loc_name, created_datetime, updated_datetime ' +
-    'FROM locations;';
-  const { rows } = await db.query(query);
-  return rows;
-};
-
-let getLocByOrg = async (org_id: string) => {
-  let query =
-    'SELECT id, org_id, loc_name, created_datetime, updated_datetime ' +
-    'FROM locations ' +
-    'WHERE org_id = $1;';
-  const { rows } = await db.query(query, [org_id]);
-  return rows;
-};
-
 router.put('/:id', async (req: Request, res: Response, next: NextFunction) => {
-  const id = req.params.id;
-
-  if (!id) {
-    return next({ error: 'location id is not defined' });
-  }
-
   try {
-    let { loc_name } = req.body;
+    const id = req.params.id;
+    const { org_id, loc_name } = req.body;
 
-    let selectQuery =
-      'SELECT id, org_id, loc_name ' + 'FROM locations ' + 'WHERE id = $1;';
-    let selectResponse = await db.query(selectQuery, [id]);
-
-    if (selectResponse.rows.length === 0) {
-      return next({ error: 'location not found' });
+    if (!id || !org_id) {
+      throw new Error('location id or org_id is not defined');
     }
-
-    let thisPet = selectResponse.rows[0];
-    loc_name = loc_name || thisPet.loc_name;
-    let updated_datetime = nowISO();
-
-    let updateQuery =
-      'UPDATE locations ' +
-      'SET loc_name = $2, updated_datetime = $3' +
-      'WHERE id = $1;';
-    let { rows } = await db.query(updateQuery, [
-      id,
-      loc_name,
-      updated_datetime,
-    ]);
-    res.status(200).json({ id, updated_datetime });
+    const locId = id.trim();
+    const orgId = org_id.trim();
+    const locName = loc_name.trim();
+    const location: ILocation = {
+      id: locId,
+      org_id: orgId,
+      loc_name: locName,
+    };
+    const locationService = new LocationService();
+    const newLocation = await locationService.UpdateLocation(location);
+    res.status(200).json({ data: newLocation });
   } catch (err) {
-    console.log(err.message);
-    next(err);
+    next({ message: err, statusCode: 400 });
   }
 });
 
 router.delete(
   '/:id',
   async (req: Request, res: Response, next: NextFunction) => {
-    const id = req.params.location_id;
-
-    if (!id) {
-      return next({ error: 'location id is not defined' });
-    }
-
     try {
-      let query = 'DELETE FROM locations WHERE id = $1;';
-      await db.query(query, [id]);
+      const id = req.params.location_id;
+      const { org_id } = req.body;
+
+      if (!id) {
+        throw new Error('location id is not defined');
+      }
+      const locId = id.trim();
+      const orgId = org_id.trim();
+      const location: ILocation = {
+        id: locId,
+        org_id: orgId,
+      };
+      const locationService = new LocationService();
+      await locationService.DeleteLocation(location);
       res.status(200).json({});
     } catch (err) {
-      next(err);
+      next({ message: err, statusCode: 400 });
     }
   }
 );
