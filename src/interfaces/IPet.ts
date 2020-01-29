1d6df7bf644bd2f93a51251f1fddb8bfb1ab5e79
Josiah Mortenson <josiah.mortenson@gmail.com>
2020-01-28 22:22:32 -0600

    first commit

---
+++ src/interfaces/IPet.ts
@@ -0,0 +1,10 @@
+export interface IPet {
+  pet_name: string;
+  id?: string;
+  user_email: string;
+  birth_date?: string;
+  created_datetime?: string;
+  updated_datetime?: string;
+  pet_type?: string;
+  breed?: string;
+}
